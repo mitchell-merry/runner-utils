4df445ca2229b6febe127d6cec633bf7077703f1
Mitchell Merry <mitchellmerry145@gmail.com>
2025-11-16 18:35:12 +1100

    title screen skip mod

---
+++ RunnerUtils/Components/TitleScreenSkip.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using Progress;
+
+namespace RunnerUtils.Components;
+
+public class TitleScreenSkip
+{
+    [HarmonyPatch(typeof(ProgressManager), "ShouldDisplayGameIntroOnStart")]
+    public static class PatchTitleScreen
+    {
+        [HarmonyPrefix]
+        public static bool Prefix()
+        {
+            Mod.Logger.LogInfo("title screen: returning false");
+            return false;
+        }
+    }
+}
